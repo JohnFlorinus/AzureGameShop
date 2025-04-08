f751ea3f5c22b8120f4e990a435baa9afc90ce5e
John Florinus <johnflorinus@gmail.com>
2025-04-08 09:52:43 +0200

    app insights added in repo

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
 using GameStore.Services;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Lägg till tjänster för MVC och GameService
 builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<GameService>();
+builder.Services.Configure<Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration>(config =>
+{
+config.SetAzureTokenCredential(new DefaultAzureCredential());
+});
+
+builder.Services.AddApplicationInsightsTelemetry(new Microsoft.ApplicationInsights.AspNetCore.Extensions.ApplicationInsightsServiceOptions
+{
+    ConnectionString = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]
+});
 
 var app = builder.Build();
 
